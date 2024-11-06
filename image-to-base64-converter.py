ef91eb82d9b7eb7b88ff04c71b7c91190ea2074a
Zach <zach.yazid03@gmail.com>
2024-11-06 16:28:42 +0800

    Created image to base64 converter script

---
+++ image-to-base64-converter.py
@@ -0,0 +1,11 @@
+import base64
+
+def image_to_base64(image_path):
+    with open(image_path, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+        return encoded_string.decode('utf-8')
+    
+image_path = "stored_faces/zach-selfie.jpg"
+base64_string = image_to_base64(image_path)
+
+print(base64_string)
